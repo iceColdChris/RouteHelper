3e2931e9468f39c0e7b991f29659268917b1b6d9
Chris Fahlin <chris_fahlin@comcast.net>
2016-08-08 18:44:15 -0700

    Started working on the register user screen

--- app/src/main/java/icecoldchris/routehelper/Controller/RegisterActivity.java
+++ app/src/main/java/icecoldchris/routehelper/Controller/RegisterActivity.java
@@ -1,15 +1,118 @@
 package icecoldchris.routehelper.Controller;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import icecoldchris.routehelper.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends Activity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Firebase.setAndroidContext(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("AUTH:", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("AUTH:", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    public void onRegister(View view) {
+        String email = ((EditText)findViewById(R.id.reg_email))
+                .getText().toString();
+        String password =((EditText)findViewById(R.id.reg_password))
+                .getText().toString();
+        String confirmPassword =((EditText)findViewById(R.id.reg_confirm_password))
+                .getText().toString();
+
+
+        if(isValidEmail(email)) {
+            if(password.equals(confirmPassword)) {
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d("AUTH:", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, "Could not create user!",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    //TODO: Do stuff! Move over to the google maps screen!
+                                    System.out.println("User Created!");
+                                }
+
+                            }
+                        });
+
+            }else{
+                Toast.makeText(RegisterActivity.this, "Please check that your passwords match!", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(RegisterActivity.this, "Please enter a valid email!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onCancel(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private boolean isValidEmail(String enteredEmail){
+        String EMAIL_REGEX = "^[\\\\w!#$%&’*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(enteredEmail);
+        return ((!enteredEmail.isEmpty()) && (matcher.matches()));
+    }
+
 }
